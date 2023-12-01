774dab7a4a5870921f28b8ee6ced54a3e7cffe3f
Ismael <ismaelalcaladev@gmail.com>
2023-12-01 14:01:23 +0100

    Modificación final del archivo Config.php

--- app/Config.php
+++ app/Config.php
@@ -2,10 +2,10 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos2023";
-     static public $mvc_bd_usuario  = "equipo2";
-     static public $mvc_bd_clave    = "123456789";
+     static public $mvc_bd_hostname = "amadorsabido.com.mialias.net/practica-despliegue-grupo2";
+     static public $mvc_bd_nombre   = "alimentos22023";
+     static public $mvc_bd_usuario  = "myamadorsa89";
+     static public $mvc_bd_clave    = "zL80v0OZ";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
