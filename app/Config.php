4154da62882fd5cee6ad67d2fd017e930f48ff12
Ismael <ismaelalcaladev@gmail.com>
2023-12-01 13:21:44 +0100

    Actualizar config de prueba

--- app/Config.php
+++ app/Config.php
@@ -3,9 +3,9 @@
  class Config
  {
      static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos2020";
-     static public $mvc_bd_usuario  = "myjesus95";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_bd_nombre   = "alimentos2023";
+     static public $mvc_bd_usuario  = "equipo2";
+     static public $mvc_bd_clave    = "123456789";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
